bacd95d1dc564657d9ff6070358b9b5ad93adbce
klaudas <klaudasj@gmail.com>
2022-06-09 21:20:35 +0300

    cleanup

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
 // swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
 	name: "ReaderKit",
 	dependencies: [
-		.Package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
-		.Package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
-		.Package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", majorVersion: 0, minor: 6),
-		.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
-		.Package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
-		.Package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.4.0"),
+		.package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
+		.package(url: "https://github.com/tadija/AEXML.git", from: "4.2.0"),
+		.package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "5.3.0"),
+		.package(url: "https://github.com/realm/realm-cocoa.git", from: "10.28.0"),
 	]
 )
